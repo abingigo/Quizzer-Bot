504ea01d75a233d66a29327de4e4c27affb5c604
playerzero <62868336+abingigo@users.noreply.github.com>
2021-03-26 23:14:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const Discord = require('discord.js');
+const MongoClient = require('mongodb').MongoClient;
+const mongo_url = "mongodb://localhost:27017/Leaderboard";
+const url = "mongodb://localhost:27017/";
 
+// quizzer = 785928731037794375
+//quizmaster = 755106658371371069
+//admin = 789382345110454272
 
-const client = new Discord.Client();
+var qn, img;
+let ans = new String();
+
+MongoClient.connect(mongo_url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created");
+    db.close();
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Leaderboard");
+    dbo.collection("Points").deleteMany({}, function(err, obj) {
+      if (err) throw err;
+      db.close();
+    });
+});
+
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix = '$';
 
@@ -24,49 +48,41 @@ client.once('ready', () => {
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    var no;
+    var args = message.content.slice(prefix.length).split(/ +/);
+    var command = args.shift().toLowerCase();
     
     if(command === 'a')
     {
-        fs.readFile('Questions.txt', (err, data) => {
-            if(err) throw err;
-            var s = data.toString().split('\n');
-            console.log(no);
-            for(i = 0; i < s.length; i++)
-            {
-                if(s[i].localeCompare(no))
-                {
-                    var ans = s[i+2];
-                    break;
-                }
-            }
-            if(ans.localeCompare(args[0]))
-                console.log("Correct Answer");
-            else
-                console.log("Wrong Answer");
-        });
+        client.commands.get('answering').execute(message, args, ans);
     }
 
-    if(command === 'q' && message.member.roles.cache.has('')) // **
+    if(command === 'q' && message.member.roles.cache.has('789382345110454272'))
     {
-        fs.readFile('Questions.txt', (err, data) => {
-            if(err) throw err;
-            var s = data.toString().split('\n');
-            no = args[0];
-            for(i = 0; i < s.length; i++)
+        var data = fs.readFileSync('Questions.txt', 'utf8');
+        var s = data.toString().split('\n');
+        no = args[0];
+        for(i = 0; i < s.length; i++)
+        {
+            if(s[i].trim() == args[0].trim())
             {
-                if(s[i].localeCompare(args[0]))
-                {
-                    var qn = s[i+1];
-                    var img = s[i+3];
-                    break;
-                }
+                qn = s[i+1];
+                ans = s[i+2];
+                img = s[i+3];
+                break;
             }
-            client.commands.get('question').execute(message, args, qn, img, Discord);
-        });
+        }
+        client.commands.get('question').execute(message, args, qn, img, Discord);
+    }
+
+    if(command == 'start' && message.member.roles.cache.has('789382345110454272'))
+    {
+        client.commands.get('reactionrole').execute(message, Discord, client);
+    }
+
+    if(command == 'end' && message.member.roles.cache.has('789382345110454272'))
+    {
+        client.commands.get('ending').execute(message, Discord, client);
     }
 });
 
-client.login('');// **
+client.login('Nzg1OTM0MjE0OTQ3OTMwMTcy.X8_ERg.EVTi59nKQY4U6unoJAUKPeq3yRI');
